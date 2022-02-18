b8f657da44e65e76480200d6adf882b7aa0b68d1
Erin Millard <ezzatron@gmail.com>
2022-02-18 20:59:04 +1000

    Improve output when releases fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,22 @@ set -e
 TAG_PATTERN="^refs/tags/(.*)$"
 SEMVER_PATTERN="^v?((0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-([0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*))?(\\+([0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*))?)$"
 
+function die {
+  echo -e "\033[0;31m$1\033[0m" >/dev/stderr
+  exit 1
+}
+
 if [[ "$GITHUB_REF" =~ $TAG_PATTERN ]]; then
   TAG=${BASH_REMATCH[1]}
 else
-  echo Cannot create a release from a non-tag >/dev/stderr
-  exit 1
+  die Cannot create a release from a non-tag
 fi
 
 # fetch the annotated tag, GHA creates a fake lightweight tag
 git fetch origin --force "$GITHUB_REF:$GITHUB_REF" >/dev/null 2>&1
 
 if [[ "$(git describe --match "$(echo "$TAG" | sed 's/./\\&/g')" "$TAG")" != "$TAG" ]]; then
-  echo Cannot create a release from a lightweight tag >/dev/stderr
-  exit 1
+  die Cannot create a release from a lightweight tag
 fi
 
 if [[ "$TAG" =~ $SEMVER_PATTERN ]]; then
@@ -41,10 +44,18 @@ fi
 
 if [[ -n "$IS_STABLE" ]]; then
   echo "$ACTION stable release for tag $TAG"
-  hub release "$COMMAND" --message "$MESSAGE" "$TAG" >/dev/null
-  echo "Published $(hub release show --format '%U' "$TAG")"
+
+  if OUTPUT="$(hub release "$COMMAND" --message "$MESSAGE" "$TAG" 2>&1)"; then
+    echo "Published $(hub release show --format '%U' "$TAG")"
+  else
+    die "Unable to publish: $OUTPUT"
+  fi
 else
   echo "$ACTION pre-release for tag $TAG"
-  hub release "$COMMAND" --prerelease --message "$MESSAGE" "$TAG" >/dev/null
-  echo "Published $(hub release show --format '%U' "$TAG")"
+
+  if OUTPUT="$(hub release "$COMMAND" --prerelease --message "$MESSAGE" "$TAG" 2>&1)"; then
+    echo "Published $(hub release show --format '%U' "$TAG")"
+  else
+    die "Unable to publish: $OUTPUT"
+  fi
 fi
