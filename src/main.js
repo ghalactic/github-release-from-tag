f7e6a6eaee5faf06b15a7d473188af93dfed19df
Erin Millard <ezzatron@gmail.com>
2022-02-19 17:23:44 +1000

    Document markdown body compromise

--- src/main.js
+++ src/main.js
@@ -80,6 +80,21 @@ async function main () {
 
   const {rest: {markdown}} = getOctokit(getInput('token'))
 
+  /**
+   * Pre-render the body using CommonMark, because tag annotations are often
+   * wrapped to a fixed column width, and GFM renders every one of these
+   * newlines as a <br> tag.
+   *
+   * This approach sucks because it means the actual release body ends up being
+   * HTML instead of the original Markdown, but I don't know of any simple way
+   * to parse the tag annotation body as CommonMark and then render it as GFM,
+   * with all of the intended line breaks intact. If you know how to do this
+   * correctly, please open an issue.
+   *
+   * Pre-rendering with CommonMark does not render issue references (like #1),
+   * but thankfully it seems like GitHub Releases will render them even inside
+   * the pre-rendered HTML.
+   */
   const renderedTagBody = await group(`Rendering tag annotation body for ${quotedTag}`, async () => {
     const {data} = await markdown.render({mode: 'markdown', text: tagBody})
     info(data)
