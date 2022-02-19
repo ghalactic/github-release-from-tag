be6ea09048bb5f9e745d9540257a4be8ddb54f67
Erin Millard <ezzatron@gmail.com>
2022-02-19 21:25:00 +1000

    Improve release annotation

--- src/main.js
+++ src/main.js
@@ -110,7 +110,7 @@ ${tagBody}
 ${renderedTagBody}`
 
   const [release, wasCreated] = await createOrUpdateRelease(repos, tag, tagSubject, releaseBody, isPreRelease)
-  notice(`Release ${wasCreated ? 'created' : 'updated'}: ${release.html_url}`, {title: `Published release: ${tagSubject}`})
+  notice(`${wasCreated ? 'Created' : 'Updated'} ${release.html_url}`, {title: `Released - ${tagSubject}`})
 }
 
 async function createOrUpdateRelease (repos, tag, name, body, isPreRelease) {
