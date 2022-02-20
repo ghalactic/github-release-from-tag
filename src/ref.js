5c3ee831d08c07d61cc102629dafd74890d0754b
Erin Millard <ezzatron@gmail.com>
2022-02-20 21:52:04 +1000

    Add SemVer pattern comment

--- src/ref.js
+++ src/ref.js
@@ -1,3 +1,8 @@
+/**
+ * Taken directly from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+ *
+ * Modified to allow for a "v" prefix
+ */
 const SEMVER_PATTERN = /^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
 
 export function parseRef (ref) {
