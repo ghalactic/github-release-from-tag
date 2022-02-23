e4bd159ad76a426f6f128bcdf2fe92a665d68f0d
Erin Millard <ezzatron@gmail.com>
2022-02-23 13:42:40 +1000

    Fix run numbers

--- test/helpers/gha.js
+++ test/helpers/gha.js
@@ -3,12 +3,12 @@ let runId
 export async function readRunId (suffix) {
   if (runId == null) {
     const {
-      GITHUB_RUN_ID: runId = 'x',
-      GITHUB_RUN_NUMBER: runNumber = 'x',
-      GITHUB_RUN_ATTEMPT: runAttempt = 'x',
+      GITHUB_RUN_ID: id = 'x',
+      GITHUB_RUN_NUMBER: number = 'x',
+      GITHUB_RUN_ATTEMPT: attempt = 'x',
     } = process.env
 
-    runId = `${runId}.${runNumber}.${runAttempt}`
+    runId = `${id}.${number}.${attempt}`
   }
 
   return runId
