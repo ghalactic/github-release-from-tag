af4aef2891968e11c7a426f00013f8fb6509528d
Erin Millard <contact@erin.id.au>
2022-09-10 12:56:36 +1000

    Fix Jest transformation issue

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ export default {
   maxWorkers: 100,
   preset: "./jest.preset.js",
   setupFilesAfterEnv: ["jest-extended/all"],
-  transformIgnorePatterns: ["src/main.js"],
+  transformIgnorePatterns: [
+    "src/main.js",
+    "signal-exit", // see https://github.com/facebook/jest/issues/9503#issuecomment-708507112
+  ],
   collectCoverageFrom: ["src/**/*"],
   coverageDirectory: "artifacts/coverage/jest",
   testMatch: ["**/test/**/*.spec.*"],
