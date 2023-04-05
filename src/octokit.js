1adccc189f10bce4eb2cbae3c4ab57f6124c60ca
Erin Millard <contact@erin.id.au>
2023-04-05 21:14:09 +1000

    Add `@octokit/plugin-retry`

--- src/octokit.js
+++ src/octokit.js
@@ -1,5 +1,8 @@
 import { Octokit } from "@octokit/action";
+import { retry } from "@octokit/plugin-retry";
 
 export function createOctokit(token) {
-  return new Octokit({ auth: token });
+  const CustomOctokit = Octokit.plugin(retry);
+
+  return new CustomOctokit({ auth: token });
 }
