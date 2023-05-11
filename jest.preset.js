915dbccfee58524b8eac598968b9b8538ac92744
Erin Millard <contact@erin.id.au>
2023-05-11 22:40:43 +1000

    Disable jest-puppeteer when E2E tests don't run

--- jest.preset.js
+++ jest.preset.js
@@ -1,7 +1,9 @@
 import esPreset from "es-jest/jest-preset.js";
 import puppeteerPreset from "jest-puppeteer/jest-preset.js";
 
+const { GITHUB_ACTIONS } = process.env;
+
 export default {
-  ...puppeteerPreset,
+  ...(GITHUB_ACTIONS == "true" ? { ...puppeteerPreset } : {}),
   ...esPreset,
 };
